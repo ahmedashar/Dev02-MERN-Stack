f3aab728970169029e77260a3f442d4cd2c53355
ahmedashar <ahmedashar12345@gmail.com>
2024-11-16 21:53:18 +0500

    add class11

---
+++ Class11 (JS)/app.js
@@ -0,0 +1,161 @@
+
+// operators
+// = --> assigning operators
+// comparision operators
+// ==
+// === check dataType also
+// >
+// <
+// <=
+// >=
+// != --> is not equal to 
+// !==
+
+// var a = 50;
+// var b = '50';
+// var c = true;
+
+// console.log(a == b);
+// console.log(a < b);
+// console.log(a <= b);
+// console.log(a != b);
+
+// console.log(a === b)
+// console.log(typeof(a))
+// console.log(typeof(b))
+// console.log(typeof(c))
+
+// logical operators
+// &&   --> AND
+// ||   --> OR
+
+// var x = true && false && true;
+// var x = true || false || false;
+
+// console.log(x)
+
+
+// var age = prompt("Enter Your Age",10);
+// console.log(age)
+
+// var a = "asd";
+
+// if (a) {
+//     console.log("Mai IF hun")
+// } else {
+//     console.log("Mai ELSE hun")
+// }
+
+
+// vote eligibility checker
+
+// var age = +prompt("How Old Are You?😀");
+// console.log(typeof(age))
+
+// if(age >= 18){
+//     alert("You are eligible to vote 😊");
+// }else{
+//     alert("You are not eligible to vote 🤨");
+// }
+
+// var trafficLight = "green";
+
+
+// if(trafficLight === "red"){
+//     console.log("Stop");
+// }
+// else if(trafficLight === "yellow"){
+//     console.log("Slow Down");    
+// }
+// else{
+//     console.log("Go");
+// }
+
+
+// Authentication Program ----------------------------
+
+// var email = "ashar@gmail.com";
+// var password = "helloworld";
+
+
+// var userEmail = prompt("Enter Your Email");
+// var userPassword = prompt("Enter Your Password");
+
+
+
+// if(userEmail ==  email && userPassword  == password){
+//     alert("You Are Login")
+// }else{
+//     alert("Invalid Email or Password")
+// }
+
+
+
+
+// You are managing a discount system for an online store. Customers are divided into members and guests, and the discounts they receive depend on their membership status and the total purchase amount. The rules are as follows:
+
+// For Members:
+
+// Purchase $100 or more: 20% discount
+// Purchase $50 to $99: 10% discount
+// Purchase less than $50: 5% discount
+// For Guests:
+
+// Purchase $100 or more: 10% discount
+// Purchase $50 to $99: 5% discount
+// Purchase less than $50: No discount (Encourage them to become a member!)
+// Write a JavaScript program using nested if statements to implement the above logic.
+
+
+// var checkUser = prompt("You have membership?", "No");
+// var purchasePrice = prompt("Enter your purchasing amount.",40)
+
+// if(checkUser === "Yes"){
+//     console.log("Member")
+//     if(purchasePrice >= 100){
+//         console.log("20% discount")
+
+//     }else if(purchasePrice >= 50 && purchasePrice <= 99){
+//         console.log("10% discount")
+//     }else{
+//         console.log("5% discount")
+//     }
+
+
+// }else{
+//     console.log("Guest")
+//     if(purchasePrice >= 100){
+//         console.log("10% discount")
+
+//     }else if(purchasePrice >= 50 && purchasePrice <= 99){
+//         console.log("5% discount")
+//     }else{
+//         console.log("No discount")
+//     }
+
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
